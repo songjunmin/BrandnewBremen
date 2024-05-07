1895deac10b14e311c37dfd86dc687366f2f94ac
Juho Jang <pukusukusu801@gmail.com>
2024-05-07 23:30:10 +0900

    feat: modify Repository to JpaRepository

--- api-server/src/main/java/com/loga/apiserver/repository/WeaponRepository.java
+++ api-server/src/main/java/com/loga/apiserver/repository/WeaponRepository.java
@@ -1,20 +1,7 @@
 package com.loga.apiserver.repository;
 
-import com.loga.apiserver.domain.Weapon;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import org.springframework.stereotype.Repository;
+import com.loga.reinforcementserver.domain.Weapon;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-@Repository
-public class WeaponRepository implements CrudRepository<Weapon, Long> {
-    @PersistenceContext
-    private EntityManager em;
-    @Override
-    public void save(Weapon entity) {
-        em.persist(entity);
-    }
-    @Override
-    public Weapon findById(Long id) {
-        return em.find(Weapon.class, id);
-    }
+public interface WeaponRepository extends JpaRepository<Weapon, Long> {
 }
